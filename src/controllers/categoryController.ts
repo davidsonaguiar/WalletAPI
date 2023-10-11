eec5deb58338076d4b36198b27766a9b192ccecb
davidson.05@outlook.com <davidson.05@outlook.com>
2023-10-11 03:46:50 -0300

    Add Crud Category

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -1,3 +1,4 @@
+import { Category } from '@prisma/client';
 import { Request, Response } from "express";
 import categoryService from "../services/categoryService";
 import userService from "../services/userService";
@@ -18,8 +19,76 @@ async function getCategories(request: Request, response: Response) {
   }
 }
 
+async function addCategory(request: Request, response: Response) {
+  const auth = request.headers.authorization;
+  const userId = auth && userService.getUserIdByToken(auth);
+  if(userId) {
+    const category: Category = await request.body;
+    category.user_id = userId;
+    try {
+      await categoryService.findCategoryByUnique(category.name, category.type, userId);
+      return response.status(400).json("Categoria já cadastrada.");
+    } catch(error) {
+      try {
+        await categoryService.saveCategory(category);
+        return response.status(201).json("Categoria cadastrada com sucesso.");
+      } catch(error) {
+        return response.status(500).json("Erro ao cadatrada categoria.");
+      }
+    }
+  }
+}
+
+async function editCategory(request: Request, response: Response) {
+  const auth = request.headers.authorization;
+  const userId = auth && userService.getUserIdByToken(auth);
+  if(userId) {
+    const categoryId: string = request.params.id;
+    const category: Category = request.body;
+    category.user_id = userId;
+    category.id = Number(categoryId);
+    try {
+      await categoryService.findCategoryByUnique(category.name, category.type, userId);
+      return response.status(400).json("Categoria já cadastrada.");
+    } catch(erro) {
+      try {
+        await categoryService.findCategoryById(Number(categoryId));
+        await categoryService.updateCategory(category);
+        return response.status(200).json("Categoria editada com sucesso.");
+      } catch(erro) {
+        return response.status(404).json("Categoria não localizada.")
+      }
+    }
+  }
+}
+
+async function removeCategory(request: Request, response: Response) {
+  const auth = request.headers.authorization;
+  const userId = auth && userService.getUserIdByToken(auth);
+  if(userId) {
+    const categoryId = request.params.id;
+
+    try {
+      await categoryService.findCategoryById(Number(categoryId));
+    } catch(error) {
+      return response.status(404).json("Categoria não localizada.");
+    }
+
+    try {
+      await categoryService.deleteCategory(Number(categoryId));
+      return response.status(200).json("Categoria deletada com sucesso.");
+    } catch(error) {
+      return response.status(400).json("Categoria vinculada a transações.");
+    }
+
+  }
+}
+
 const categoriesController = {
-  getCategories
+  getCategories,
+  addCategory,
+  editCategory,
+  removeCategory
 }
 
 export default categoriesController;
--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -1,6 +1,25 @@
+import { Category } from "@prisma/client";
 import prisma from "../prisma"
 
 const categoryRepository = prisma.category;
+
+async function findCategoryById(id: number) {
+  return categoryRepository.findUniqueOrThrow({
+    where: { id }
+  })
+}
+
+async function findCategoryByUnique(name: string, type: "Ganhos" | "Gastos", user_id: string) {
+  return categoryRepository.findUniqueOrThrow({
+    where: {
+      name_type_user_id: {
+        name,
+        user_id,
+        type
+      }
+    }
+  })
+}
 
 async function findCategories(userId: string) {
   return categoryRepository.findMany({
@@ -13,8 +32,30 @@ async function findCategories(userId: string) {
   });
 }
 
+async function saveCategory(data: Category) {
+  return categoryRepository.create({ data })
+}
+
+async function updateCategory(data: Category) {
+  return categoryRepository.update({
+    where: { id: data.id },
+    data
+  })
+}
+
+async function deleteCategory(id: number) {
+  return categoryRepository.delete({
+    where: { id }
+  })
+}
+
 const categoryService = {
-  findCategories
+  findCategoryById,
+  findCategoryByUnique,
+  findCategories, 
+  saveCategory,
+  updateCategory,
+  deleteCategory
 }
 
 export default categoryService;
